795b9385edab1e2e126a494bfaaa8eed226380e2
AjayGajula <69115878+AjayGajula@users.noreply.github.com>
2020-12-15 18:14:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const url =
-  "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
+  "https://5fc38a07e5c28f0016f54b09.mockapi.io/ajay/tableDetails";
 
 let itemList = (data) => {
   $("#tbody").append(
